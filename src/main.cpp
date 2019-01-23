8cc50c69c9d72ecfd576ee3b1cbfdf26cc16af86
Matej <dobrakmato@gmail.com>
2019-01-23 19:19:43 +0100

    rework how testing works

--- src/main.cpp
+++ src/main.cpp
@@ -10,12 +10,13 @@ int main(int argc, char *argv[]) {
         exit(1);
     }
 
-    if (strcmp(argv[1], "--tests") == 0) {
-        ::testing::InitGoogleTest(&argc, argv);
-        return RUN_ALL_TESTS();
+    auto fh = fopen(argv[1], "rb"); // we use b to be safe on windows systems
+
+    if (!fh) {
+        printf("File '%s' not found!", argv[1]);
+        exit(-1);
     }
 
-    auto fh = fopen(argv[1], "rb"); // we use b to be safe on windows systems
     fseek(fh, 0, SEEK_END);
     auto size = ftell(fh);
     fseek(fh, 0, SEEK_SET);
